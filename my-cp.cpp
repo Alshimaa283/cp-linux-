db989bde9671845a4906b02879d5a6ad4638d5fc
Alshimaa283 <m7081878@gmail.com>
2021-01-31 22:15:08 -0500

    Hello from my CP

---
+++ my-cp.cpp
@@ -0,0 +1,24 @@
+#include <unistd.h>
+#include <fcntl.h>
+int main (int argc , char **argv)
+{
+if (argc < 3)
+  return 1;
+ int fdSrcFile = open(argv[1] , O_RDONLY);
+ if (fdSrcFile == -1)
+ {
+  return 2;
+ }
+ int fdDstFile = open(argv[2] , O_CREAT | O_WRONLY , S_IRUSR | S_IWUSR);
+ if (fdDstFile == -1)
+ {
+  return 3;
+ }
+ char buffer[2];
+ ssize_t charCount;
+ while ((charCount = read(fdSrcFile , buffer , sizeof(buffer))) !=0 )
+ {
+  write(fdDstFile , buffer , charCount);
+ }
+    return 0;
+}
